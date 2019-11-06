5b009f8ab8ec909a94b665cc20da4f498f0a050c
Igor Lima <limaig@TTG.NET>
2019-11-06 20:34:39 -0300

    003-salsifufu

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,7 +1,27 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data').valor;
+        this._inputQuantidade = $('#quantidade').valor;
+        this._inputValor = $('#valor').valor;
+    }
+
     adiciona(event) {
         event.preventDefault();
-        alert('Chamei ação no controller');
+
+        let data = new Date(
+            ...this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
